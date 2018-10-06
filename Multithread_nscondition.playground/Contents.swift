c2f7751a3925065fef5f937896de70308f88fa8e
Rishat <xpan96@gmail.com>
2018-10-06 11:23:45 +0300

    Printer and writed mechanism

---
+++ Multithread_nscondition.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+// Документация https://developer.apple.com/documentation/foundation/nscondition
+var str = "NSCondition()"
+
+// Создаем пустой массив
+var array = [String]()
+
+// Поток и подключение
+var available = false
+var condition = pthread_cond_t()
+var mutex = pthread_mutex_t()
+
+// Создаем класс и наследуемся от Потока (thread'a)
+class PrinterMutex: Thread {
+    
+    override init() {
+        pthread_cond_init(&condition, nil)
+        pthread_mutex_init(&mutex, nil)
+    }
+    
+    override func main() {
+        printerMethod()
+    }
+    
+    // Создаем приватный метод, который будет называтся printer...
+    private func printerMethod() {
+        pthread_mutex_lock(&mutex)
+        
+        print("printerMethod - entered")
+        
+        while (!available) {
+            // Заставляем его поспать
+            pthread_cond_wait(&condition, &mutex)
+        }
+        available = false
+        defer {
+            // Снимаем блокировку
+            pthread_mutex_unlock(&mutex)
+        }
+        
+        print("printerMethod - exit")
+    }
+    
+}
+
+// С начала записываем , а потом вызываем принтер
+// p/s/ это другой поток
+class WriterMutex: Thread {
+    
+    override init() {
+        pthread_cond_init(&condition, nil)
+        pthread_mutex_init(&mutex, nil)
+    }
+    
+    override func main() {
+        writerMethod()
+    }
+    
+    // Создаем приватный метод, который будет называтся printer...
+    private func writerMethod() {
+        pthread_mutex_lock(&mutex)
+        
+        print("writer - entered")
+        
+        pthread_cond_signal(&condition) // Посылаем сигнал
+        
+        available = true
+        defer {
+            // Снимаем блокировку
+            pthread_mutex_unlock(&mutex)
+        }
+        
+        print("writer - exit")
+    }
+    
+}
