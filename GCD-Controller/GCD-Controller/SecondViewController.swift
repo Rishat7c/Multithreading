d4543c46f27dec49c87238e1c94cff0e1d06eb29
Rishat <xpan96@gmail.com>
2018-10-06 16:47:25 +0300

    initial two controller

---
+++ GCD-Controller/GCD-Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GCD-Controller
+//
+//  Created by Ришат Гайсин on 06/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
---
+++ GCD-Controller/GCD-Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD-Controller
+//
+//  Created by Ришат Гайсин on 06/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
