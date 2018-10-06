75c0da86d55a488c511db6904e14fd5617311919
Rishat <xpan96@gmail.com>
2018-10-06 11:52:42 +0300

    release ViewController

--- Multithread_GDC.playground/Contents.swift
+++ Multithread_GDC.playground/Contents.swift
@@ -1,3 +1,45 @@
 import UIKit
+import PlaygroundSupport // Чтобы разрешать дожидаться каждый поток
 
-var str = "Hello, playground"
+// Долгожданный GDC
+var str = "GDC"
+
+/**
+ *  1. DispatchQueue.global
+ *  2. DispatchQueue.main
+ *  async или sync поток (thread)
+ */
+
+class MyViewController: UIViewController {
+    
+    var button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Demonstration VC"
+        view.backgroundColor = UIColor.white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        initButton()
+    }
+    
+    func initButton() {
+        button.frame = CGRect(x:0,y:0,width:200,height:50)
+        button.center = view.center
+        button.setTitle("Press", for:.normal)
+        button.backgroundColor = UIColor.green
+        button.setTitleColor(UIColor.white, for: .normal)
+        view.addSubview(button)
+    }
+    
+}
+
+let vc = MyViewController()
+let navbar = UINavigationController(rootViewController: vc)
+
+PlaygroundPage.current.liveView = navbar
+
+
+
